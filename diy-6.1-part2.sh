b929b5d4acee489d62110f54f2330e439e25fe69
ximiTech <63538901+ximiTech@users.noreply.github.com>
2023-01-03 18:25:39 +0800

    Update diy-6.1-part2.sh

--- diy-6.1-part2.sh
+++ diy-6.1-part2.sh
@@ -39,7 +39,7 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-a
 # git clone https://github.com/pymumu/smartdns.git package/smartdns
 
 ########### 安装解锁网易云 ###########
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+git clone -b master https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 
 ########### 安装AdguardHome ###########
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
