a4476162446331fe28906ec03ac32768c254b5b6
ximiTech <63538901+ximiTech@users.noreply.github.com>
2023-02-04 00:23:54 +0800

    Update diy-5.10-part2.sh

--- diy-5.10-part2.sh
+++ diy-5.10-part2.sh
@@ -18,7 +18,7 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 ########### 更改大雕源码（可选）###########
-sed -i "s/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.10/g" target/linux/x86/Makefile
+sed -i "s/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=5.10/g" target/linux/x86/Makefile
 
 ########### 更改默认主题（可选）###########
 # 删除自定义源默认的 argon 主题
@@ -31,17 +31,17 @@ sed -i "s/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.10/g" target/linux/x86/Makefil
 
 ########### 更新lean的内置的smartdns版本 ###########
 sed -i 's/1.2022.38/1.2022.40/g' feeds/packages/net/smartdns/Makefile
-sed -i "s/PKG_SOURCE_VERSION:=*/PKG_SOURCE_VERSION:=d6f9b07f1ce9462c7775cd7531db28809b010903/g" feeds/packages/net/smartdns/Makefile
-sed -i "s/PKG_MIRROR_HASH:=*/PKG_MIRROR_HASH:=skip/g" feeds/packages/net/smartdns/Makefile
+sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=d6f9b07f1ce9462c7775cd7531db28809b010903/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/PKG_MIRROR_HASH:=.*/PKG_MIRROR_HASH:=skip/g' feeds/packages/net/smartdns/Makefile
 
 ########### 安装smartdns（必选）###########
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 # git clone https://github.com/pymumu/smartdns.git package/smartdns
 
 ########### 维持xray-core的版本 ###########
-sed -i "s/PKG_VERSION:=1.*.*/PKG_VERSION:=1.7.2/g" feeds/passwall_packages/xray-core/Makefile
-sed -i "s/PKG_RELEASE:=*/PKG_RELEASE:=1/g" feeds/passwall_packages/xray-core/Makefile
-sed -i "s/PKG_HASH:=*/PKG_HASH:=skip/g" feeds/passwall_packages/xray-core/Makefile
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.7.2/g' feeds/passwall_packages/xray-core/Makefile
+sed -i 's/PKG_RELEASE:=.*/PKG_RELEASE:=1/g' feeds/passwall_packages/xray-core/Makefile
+sed -i 's/PKG_HASH:=.*/PKG_HASH:=skip/g' feeds/passwall_packages/xray-core/Makefile
 
 ########### 安装解锁网易云 ###########
 git clone -b master https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
