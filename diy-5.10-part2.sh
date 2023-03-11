31118e8ca7d7b552fb98c9eb6ee7fbf6f9fe2eb4
ximiTech <63538901+ximiTech@users.noreply.github.com>
2023-03-11 13:59:49 +0800

    Update diy-5.10-part2.sh

--- diy-5.10-part2.sh
+++ diy-5.10-part2.sh
@@ -31,7 +31,7 @@ sed -i "s/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=5.10/g" target/linux/x86/Makefile
 
 ########### 更新lean的内置的smartdns版本 ###########
 sed -i 's/1.2022.38/1.2022.41/g' feeds/packages/net/smartdns/Makefile
-sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=c42f98979c61d91b91c39dbc82a8056dc8579ad2/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=3c7b86eea755cf726a49d452a7b43ceeb01f2a06/g' feeds/packages/net/smartdns/Makefile
 sed -i 's/PKG_MIRROR_HASH:=.*/PKG_MIRROR_HASH:=skip/g' feeds/packages/net/smartdns/Makefile
 
 ########### 安装smartdns（必选）###########
