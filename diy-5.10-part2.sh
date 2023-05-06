0571c8b3757b7a9fe5a11a37cf0bf7717fca7f3c
ximiTech <63538901+ximiTech@users.noreply.github.com>
2023-05-06 21:11:27 +0800

    Update diy-5.10-part2.sh

--- diy-5.10-part2.sh
+++ diy-5.10-part2.sh
@@ -31,7 +31,7 @@ sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
 
 ########### 更新lean的内置的smartdns版本 ###########
 # sed -i 's/1.2022.38/1.2023.41/g' feeds/packages/net/smartdns/Makefile
-sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=56d0332bf91104cfc877635f6c82e9348587df04/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=ccadadd3fb710c23e9cbbd48fd396b19580427fd/g' feeds/packages/net/smartdns/Makefile
 sed -i 's/PKG_MIRROR_HASH:=.*/PKG_MIRROR_HASH:=skip/g' feeds/packages/net/smartdns/Makefile
 
 ########### 维持xray-core的版本 ###########
